b4e7fae324aa11f861a395ab406104bc34b36833
Robert Downs <downs.w.robert@gmail.com>
2025-09-23 10:40:35 -0400

    Adding Unit tests for both PersonController and AstronautDutyController

--- StargateTests/Controllers/AstronautDutyControllerTests.cs
+++ StargateTests/Controllers/AstronautDutyControllerTests.cs
@@ -1,12 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using StargateAPI.Business.Commands;
+using StargateAPI.Business.Queries;
+using StargateAPI.Controllers;
+using System.Net;
 
-namespace StargateTests.Controllers
+namespace StargateAPI.Tests.Controllers
 {
-    internal class AstronautDutyControllerTests
+    public class AstronautDutyControllerTests
     {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly AstronautDutyController _controller;
+
+        public AstronautDutyControllerTests()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _controller = new AstronautDutyController(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task GetAstronautDutiesByName_ReturnsOk_WhenMediatorReturnsSuccess()
+        {
+            // Arrange
+            var name = "JonasQuinn";
+            var response = new GetPersonByNameResult
+            {
+                Success = true,
+                Message = "Found",
+                ResponseCode = (int)HttpStatusCode.OK
+            };
+
+            _mediatorMock
+                .Setup(m => m.Send(It.IsAny<GetPersonByName>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAstronautDutiesByName(name) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal((int)HttpStatusCode.OK, result.StatusCode);
+            Assert.Equal(response, result.Value);
+        }
+
+        [Fact]
+        public async Task GetAstronautDutiesByName_Returns500_OnException()
+        {
+            // Arrange
+            var name = "Elizabeth Weir";
+            _mediatorMock
+                .Setup(m => m.Send(It.IsAny<GetPersonByName>(), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new Exception("Something went wrong"));
+
+            // Act
+            var result = await _controller.GetAstronautDutiesByName(name) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            var response = Assert.IsType<BaseResponse>(result.Value);
+            Assert.False(response.Success);
+            Assert.Equal("Something went wrong", response.Message);
+            Assert.Equal((int)HttpStatusCode.InternalServerError, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateAstronautDuty_ReturnsOk_WhenMediatorReturnsSuccess()
+        {
+            // Arrange
+            var request = new CreateAstronautDuty
+            {
+                Name = "John Shepard",
+                Rank = "Lt Colonel",
+                DutyTitle = "Lunar Module Pilot",
+                DutyStartDate = DateTime.MinValue // or any appropriate date
+            };
+
+            var response = new CreateAstronautDutyResult
+            {
+                Success = true,
+                Message = "Created",
+                ResponseCode = (int)HttpStatusCode.OK
+            };
+
+            _mediatorMock
+                .Setup(m => m.Send(It.IsAny<CreateAstronautDuty>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.CreateAstronautDuty(request) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal((int)HttpStatusCode.OK, result.StatusCode);
+            Assert.Equal(response, result.Value);
+        }
     }
 }
--- StargateTests/Controllers/PersonControllerTests.cs
+++ StargateTests/Controllers/PersonControllerTests.cs
@@ -1,12 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using StargateAPI.Business.Commands;
+using StargateAPI.Business.Queries;
+using StargateAPI.Controllers;
+using System.Net;
 
-namespace StargateTests.Controllers
+namespace StargateAPI.Tests.Controllers
 {
-    internal class PersonControllerTests
+    public class PersonControllerTests
     {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly PersonController _controller;
+
+        public PersonControllerTests()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _controller = new PersonController(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task GetPeople_ReturnsOk_WhenMediatorReturnsSuccess()
+        {
+            // Arrange
+            var response = new GetPeopleResult { Success = true, Message = "ok", ResponseCode = (int)HttpStatusCode.OK };
+
+            _mediatorMock
+                .Setup(m => m.Send(It.IsAny<GetPeople>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetPeople();
+
+            // Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal((int)HttpStatusCode.OK, objectResult.StatusCode);
+            Assert.Equal(response, objectResult.Value);
+        }
+
+        [Fact]
+        public async Task GetPersonByName_ReturnsOk_WhenMediatorReturnsSuccess()
+        {
+            // Arrange
+            var response = new GetPersonByNameResult { Success = true, Message = "found", ResponseCode = (int)HttpStatusCode.OK };
+            _mediatorMock
+                .Setup(m => m.Send(It.Is<GetPersonByName>(q => q.Name == "Samantha Carter"), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetPersonByName("Samantha Carter");
+
+            // Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal((int)HttpStatusCode.OK, objectResult.StatusCode);
+            Assert.Equal(response, objectResult.Value);
+        }
+
+        [Fact]
+        public async Task CreatePerson_ReturnsOk_WhenMediatorReturnsSuccess()
+        {
+            // Arrange
+            var response = new CreatePersonResult { Success = true, Message = "created", ResponseCode = (int)HttpStatusCode.Created };
+            _mediatorMock
+                .Setup(m => m.Send(It.Is<CreatePerson>(c => c.Name == "Daniel Jackson"), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.CreatePerson("Daniel Jackson");
+
+            // Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal((int)HttpStatusCode.Created, objectResult.StatusCode);
+            Assert.Equal(response, objectResult.Value);
+        }
+
+        [Fact]
+        public async Task CreatePerson_ReturnsConflict_WhenPersonAlreadyExists()
+        {
+            // Arrange
+            var response = new CreatePersonResult
+            {
+                Success = false,
+                Message = "Person already exists",
+                ResponseCode = (int)HttpStatusCode.Conflict
+            };
+
+            _mediatorMock
+                .Setup(m => m.Send(It.Is<CreatePerson>(c => c.Name == "Jack O'Neill"), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.CreatePerson("Jack O'Neill");
+
+            // Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal((int)HttpStatusCode.Conflict, objectResult.StatusCode);
+
+            var returnedResponse = Assert.IsType<CreatePersonResult>(objectResult.Value);
+            Assert.False(returnedResponse.Success);
+            Assert.Equal("Person already exists", returnedResponse.Message);
+        }
     }
 }
